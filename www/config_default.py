d84059ce33e2a513ee5ed684b32e30cf9f1c95c3
Dman <dmankacio@163.com>
2019-04-25 11:19:52 +0800

    day7 mvc 基本完成，下面是调试

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+__author__ = 'Dman'
+
+configs = {
+    'db': {
+        'user': 'root',
+        'pwd': 'abc123',
+        'db': 'apw',
+        'host': 'heli.dman.me',
+        'port': 3306
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,14 @@
+#config_override.py
+
+configs = {
+    'db': {
+        'user': 'root',
+        'pwd': 'abc123',
+        'db': 'apw',
+        'host': 'heli.dman.me',
+        'port': 3306
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
